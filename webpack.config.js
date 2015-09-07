3b25416edaf2d2892844412ac20038bed4bc3ec6
carrotflakes <carrotflakes@gmail.com>
2015-09-08 04:17:55 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: {
+    build: './src/snakeParser.js',
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "snakeParser.js",
+    sourcePrefix: "",
+  }
+};
+
