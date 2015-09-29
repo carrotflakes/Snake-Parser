700e5de1070cff5e12a4ad635161682a29c72af4
carrotflakes <carrotflakes@gmail.com>
2015-09-29 17:56:37 +0900

    Implement jsLiteralify
    jsLiteralify is similar to JSON.stringify.
    jsLiteralify can handle 'undefined' object.

---
+++ src/jsLiteralify.js
@@ -0,0 +1,37 @@
+function stringLiteralify(string) {
+	return JSON.stringify(string)
+		.replace(/\u2028/g, "\\u2028")
+		.replace(/\u2029/g, "\\u2029");
+}
+
+var objectToString = Object.prototype.toString;
+
+function jsLiteralify(object) {
+	if (object === null)
+		return "null";
+
+	switch (typeof object) {
+	case "string":
+		return stringLiteralify(object);
+
+	case "number":
+	case "boolean":
+		return JSON.stringify(object);
+
+	case "undefined":
+		return "undefined";
+	}
+
+	switch (objectToString.call(object)) {
+	case "[object Object]":
+		var members = Object.keys(object).map(function (key) {
+			return stringLiteralify(key) + ":" + jsLiteralify(object[key]);
+		});
+		return "{" + members.join(",") + "}";
+
+	case "[object Array]":
+		return "[" + object.map(jsLiteralify).join(",") + "]";
+	}
+}
+
+module.exports = jsLiteralify;
